00131494acb52b5cc31ec092726b303d5db226cb
saimun5656 <shaounlosted@gmail.com>
2023-05-03 16:25:13 +0600

    json addedd

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express =require('express');
+const chefs=require('./data/chefs.json')
 const app=express();
 const port=5000;
 app.get('/',(req,res)=>{
     res.send('server running')
+})
+app.get('/chefs',(req,res)=>{
+    
+    res.send(chefs)
 })
 app.listen(port,()=>{
     console.log(`server is running at ${port}`)
