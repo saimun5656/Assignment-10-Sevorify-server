d903a936c3a1e86e9361a325614a69c261b57940
saimun5656 <shaounlosted@gmail.com>
2023-05-04 13:23:21 +0600

    recipes added

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express =require('express');
 const cors =require('cors')
 const chefs=require('./data/chefs.json')
+const categories=require('./data/categories.json')
+const recipes=require('./data/recipes.json')
 const app=express();
 const port=5000;
 app.use(cors());
@@ -8,8 +10,18 @@ app.get('/',(req,res)=>{
     res.send('server running')
 })
 app.get('/chefs',(req,res)=>{
-    
     res.send(chefs)
+})
+app.get('/chef/:id',(req,res)=>{
+    const id=parseInt(req.params.id);
+    const selectedChef=chefs.find(cf=>parseInt(cf.id)===id);
+    res.send(selectedChef)
+})
+app.get('/categories',(req,res)=>{
+    res.send(categories)
+})
+app.get('/recipes',(req,res)=>{
+    res.send(recipes)
 })
 app.listen(port,()=>{
     console.log(`server is running at ${port}`)
